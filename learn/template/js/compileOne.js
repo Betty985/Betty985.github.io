121419a8b5541c575e0f7edbaa0d9c51ac1783bd
Betty985 <1830265753@qq.com>
2021-11-13 17:16:46 +0800

    "fix"

--- learn/template/js/compileOne.js
+++ learn/template/js/compileOne.js
@@ -1,1 +1,26 @@
-function compile(dom, data) {}
+function compile(dom, data) {
+  //   Object.defineProperties(data, {
+  //     get: a(dom, data),
+  //     set: a(dom, data),
+  //     enumerable: true,
+  //     configurable: true,
+  //   });
+  //   function a(dom, data) {
+  let expm = /{{msg}}|"msg"/g;
+  let expn = /{{name}}|"name"/g;
+  let expt = /{{test}}|"test"/g;
+  //   let m = expm.exec(dom.innerText);
+  //   let n = expn.exec(dom.innerText);
+  //   let t = expt.exec(dom.innerText);
+  //   let a = new Map([[,data.name],[,data.msg],[,data.test]);
+  //   m.splice(0, 0, data.msg)[0];
+  //   data.msg = m[0];
+  //   n.splice(0, 0, data.name)[0];
+  //   data.name = n[0];
+  //   t.splice(0, 0, data.test)[0];
+  //   data.test = t[0];
+  dom.innerHTML = dom.innerHTML.replace(expm, `${data.msg}`);
+  dom.innerHTML = dom.innerHTML.replace(expn, `${data.name}`);
+  dom.innerHTML = dom.innerHTML.replace(expt, `${data.test}`);
+  //   }
+}
---
+++ learn/默写/foreach.js
@@ -0,0 +1,11 @@
+/*
+    todo:[1,2,3]
+    [1,2,3]
+    forEach([1,2,3],(index,value,arr)=>{
+        console.log(index,value)
+    })
+    //0 1
+    //1 2
+    //2 3
+*/
+function forEach() {}
