015d0a4aa4c5628271a717ed5789c3b05c4a9d2b
Betty985 <1830265753@qq.com>
2021-11-13 16:25:17 +0800

    修改日历显示问题

--- learn/debounce/js/calendar.js
+++ learn/debounce/js/calendar.js
@@ -15,23 +15,30 @@ window.onload = function () {
   let m1 = expression.exec(text);
   let m2 = expression.exec(text);
   function fillNumber(a, b) {
+    console.log(a, b);
     // 当月第一天是星期几，月份从0开始，所以需要减一
     let ddate1 = new Date(a, b - 1, 1).getDay();
+    // 0是周日，6是周六
+    ddate1 = ddate1 == 0 ? 7 : ddate1;
     // 当月天数
     let ddate2 = new Date(a, b, 0).getDate();
+    console.log(ddate1, ddate2);
     // 上月天数
     let ddate3 = new Date(a, b - 1, 0).getDate();
+    // console.log(`上月天数是${ddate3}`);
     for (let i = 0, j = ddate3 - ddate1 + 2; i < ddate1 - 1; i++, j++) {
-      day[i].innerText = j;
-    }
-    for (let i = ddate1 - 1, j = 1; j <= ddate2; i++, j++) {
-      day[i].innerText = j;
-    }
-    for (let i = ddate2, j = 1; i < 35; i++, j++) {
       day[i].innerText = j;
       day[i].style.color = "#D3D3D3";
     }
-    console.log(ddate1, ddate2, ddate3);
+    for (let i = ddate1 - 1, j = 1; j <= ddate2; i++, j++) {
+      day[i].innerText = j;
+      day[i].style.color = "#555";
+    }
+    for (let i = ddate1 + ddate2 - 1, j = 1; i < 42; i++, j++) {
+      day[i].innerText = j;
+      day[i].style.color = "#D3D3D3";
+      // console.log(`下月日期${j}`);
+    }
   }
   fillNumber(m1[0], m2[0]);
   // ======================================================================
@@ -41,7 +48,7 @@ window.onload = function () {
   // DOM2方式的主要优势是可以为同一个事件添加多个事件处理程序。
   pre.addEventListener("click", function lessen() {
     if (m2[0] > 1) {
-      cdate.innerText = `${m1[0]} 年${m2[0]--} 月`;
+      cdate.innerText = `${m1[0]} 年${--m2[0]} 月`;
     } else {
       m2[0] = 12;
       m1[0] -= 1;
@@ -51,8 +58,8 @@ window.onload = function () {
     // cdate.style.color = "red";
   });
   next.addEventListener("click", function grow() {
-    if (m2[0] < 13) {
-      cdate.innerText = `${m1[0]} 年${m2[0]++} 月`;
+    if (m2[0] < 12) {
+      cdate.innerText = `${m1[0]} 年${++m2[0]} 月`;
     } else {
       m2[0] = 1;
       m1[0]++;
